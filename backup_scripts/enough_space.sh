bfcbfb89ec17953be1b8b1e286ae779b9d2c38c9
thomasdale <tom.dale@fivium.co.uk>
2021-02-10 13:25:17 +0000

    Update enough_space.sh
    use compression ratio

--- backup_scripts/enough_space.sh
+++ backup_scripts/enough_space.sh
@@ -48,30 +48,52 @@ DATABASE_SIZE_BYTES=$(sqlplus -S / as sysdba << EOF
 EOF
 )
 #
+# Archivelog sizes
+#
+ARCHIVELOG_SIZE_BYTES=$(sqlplus -S / as sysdba << EOF
+  set head off
+  set feedback off
+  set pagesize 0
+  set linesize 300
+  SELECT SUM(blocks*block_size) archivelogs_size_bytes FROM V\$archived_log l WHERE deleted = 'NO';
+  exit
+EOF
+)
+#
 # Do we have a last backup on the filesystem to use for space check?
 #
 if [ "$LAST_BACKUP_SIZE_COMPRESSED" -gt 0 ]
 then
   #
-  # Use the compressed file system size
+  # Use the compressed file system size to work out expected compression
   # if its uncompressed, this value will also be correct
   #
-  SPACE_REQUIRED="$LAST_BACKUP_SIZE_COMPRESSED"
+  COMPRESSION_PERCENT=$(( ${LAST_BACKUP_SIZE_COMPRESSED}*100/${LAST_BACKUP_SIZE} ))
+
 else
   #
-  # Check if there is enough room for the current size of the database
+  # assume no compression
   #
-  SPACE_REQUIRED="$DATABASE_SIZE_BYTES"
+  COMPRESSION_PERCENT=100
 fi
-# 
-# Safety buffer is 20%
 #
-BUFFER_SIZE=$(( ${SPACE_REQUIRED}*20/100 ))
+# Check if there is enough room for the current size of the database
+# and archivelogs
+#
+SPACE_REQUIRED=$(( $DATABASE_SIZE_BYTES + $ARCHIVELOG_SIZE_BYTES ))
+#
+# Space with compression
+#
+SPACE_REQUIRED_WITH_COMPRESSION=$(( ${SPACE_REQUIRED}*${COMPRESSION_PERCENT}/100 ))
+#
+# Safety buffer is 5%
+#
+BUFFER_SIZE=$(( ${SPACE_REQUIRED_WITH_COMPRESSION}*5/100 ))
 DEST_DIR=$BACKUP_DIR
 ## Get available space in DEST_DIR
 SPACE_AVAILABLE=$(df $DEST_DIR -B1 -P | awk '{print $2}' | sed -e /1-blocks/d)
 ## Add buffer size
-SPACE_REQUIRED_PLUS_BUFFER=$(($SPACE_REQUIRED+$BUFFER_SIZE))
+SPACE_REQUIRED_PLUS_BUFFER=$(( ${SPACE_REQUIRED_WITH_COMPRESSION}+${BUFFER_SIZE} ))
 DELTA=$(($SPACE_AVAILABLE-$SPACE_REQUIRED_PLUS_BUFFER))
 ## Details
 echo "--------------------"
@@ -80,9 +102,12 @@ echo "--------------------"
 echo "All sizes in bytes"
 printf "Last backup size on disk            : %16s\n" "$LAST_BACKUP_SIZE"
 printf "Last backup size on disk compressed : %16s\n" "$LAST_BACKUP_SIZE_COMPRESSED"
-printf "current database size               : %16s\n" "$DATABASE_SIZE_BYTES"
+printf "Last backup compression percent     : %16s\n" "$COMPRESSION_PERCENT"
+printf "Current database size               : %16s\n" "$DATABASE_SIZE_BYTES"
+printf "Archivelog size                     : %16s\n" "$ARCHIVELOG_SIZE_BYTES"
 printf "space required                      : %16s\n" "$SPACE_REQUIRED"
-printf "20 Percent buffer                   : %16s\n" "$BUFFER_SIZE"
+printf "space required with compression     : %16s\n" "$SPACE_REQUIRED_WITH_COMPRESSION"
+printf "5 Percent buffer                    : %16s\n" "$BUFFER_SIZE"
 printf "space required with buffer          : %16s\n" "$SPACE_REQUIRED_PLUS_BUFFER"
 printf "space available                     : %16s\n" "$SPACE_AVAILABLE"
 printf "free space after backup             : %16s\n" "$DELTA"
