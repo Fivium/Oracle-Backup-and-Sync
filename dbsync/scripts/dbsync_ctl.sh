535bf2bb70a4158fe6d7629ca178dd6e6a48f223
thomasdale <tom.dale@fivium.co.uk>
2022-06-13 08:13:42 +0100

    remove corrupt files changed
    would not have worked before

--- dbsync/scripts/dbsync_ctl.sh
+++ dbsync/scripts/dbsync_ctl.sh
@@ -195,8 +195,8 @@ echo ""
 echo "Delete any corrupt archivelogs"
 echo ""
 
-grep -B 1 -i corrupted "$LOGFILE"                    | grep 'File Name' | awk '{print $3}' | sort -u | grep '.arc' | grep -v '.arc.gz' | xargs rm
-grep -B 1 "Foreign database file DBID: 0" "$LOGFILE" | grep 'File Name' | awk '{print $3}' | sort -u | grep '.arc' | grep -v '.arc.gz' | xargs rm
+grep -B 1 -i corrupted "$LOGFILE"                    | grep 'File Name' | awk '{print $3}' | sort -u | grep '.arc' | grep -v '.arc.gz' | sed -e 's/File Name: //g' | xargs rm -v
+grep -B 1 "Foreign database file DBID: 0" "$LOGFILE" | grep 'File Name' | awk '{print $3}' | sort -u | grep '.arc' | grep -v '.arc.gz' | sed -e 's/File Name: //g' | xargs rm -v
 
 PRIMARY_LOGFILE_DIR="${PRIMARY_LOGFILE_BASE_DIR}/${STANDBY_SERVER}__${ORASID}"
 
