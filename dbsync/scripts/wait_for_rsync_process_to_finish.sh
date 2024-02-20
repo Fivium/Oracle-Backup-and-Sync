adece23a7d2bc459011d843acec03a83aab64dfe
thomasdale <tom.dale@fivium.co.uk>
2024-02-20 08:52:20 +0000

    Update wait_for_rsync_process_to_finish.sh
    update to have and exclude option

--- dbsync/scripts/wait_for_rsync_process_to_finish.sh
+++ dbsync/scripts/wait_for_rsync_process_to_finish.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-SLEEP_SECONDS=5
-
-if [ $# -ne 1 ]
+if [ $# -lt 1 ]
 then
-    echo "Wrong argument count, Usage: wait_for_rsync_process_to_finish.sh <SYNC_TO_TO_DIR>"
+    echo "Wrong argument count, Usage: wait_for_rsync_process_to_finish.sh <SYNC_TO_TO_DIR> [EXCLUDE]"
     exit 1
 fi
 
+SLEEP_SECONDS=5
 DESTINATION_PATH=$1
+EXCLUDE=$2
 #
 # Check path does not have trailing /
 # we will use this to check the rsync is just to this directory
@@ -21,7 +21,10 @@ if [ $DESTINATION_PATH_LAST_CHAR = '/' ]; then
 fi
 
 echo ""
-echo "Wait for rsync process for this databases archivelogs to finish"
+echo "Wait for rsync process to $DESTINATION_PATH to finish"
+if [ -n "$EXCLUDE" ]; then
+    echo "don't wait for any files with '${EXCLUDE}' in the filename and path"
+fi
 
 function SET_RSYNC_PROCESS_COUNT()
 {
@@ -29,10 +32,9 @@ function SET_RSYNC_PROCESS_COUNT()
     date
     echo ""
     #
-    # The grep -v is remove processes syncing to further subdirectories
-    # eg $DESTINATION_PATH/archivelog_backups
+    # CMD to check for the rsync process running and syncing to path
     #
-    CHECK_PROCESS_CMD="ps -ef | grep 'rsync --server' | grep $DESTINATION_PATH | grep -v '${DESTINATION_PATH}.*/[a-zA-Z0-9]\+'"
+    CHECK_PROCESS_CMD="ps -ef | grep 'rsync --server' | grep $DESTINATION_PATH"
     RSYNC_PROCESS_COUNT=$(eval "$CHECK_PROCESS_CMD | wc -l")
     eval $CHECK_PROCESS_CMD
 }
@@ -44,8 +46,21 @@ do
    echo ""
    echo "rsync to $DESTINATION_PATH processes running : $RSYNC_PROCESS_COUNT"
    echo ""
-   find ${DESTINATION_PATH} -type f -iname ".*" -ls | awk '{print $7" bytes "$11}'
+   find ${DESTINATION_PATH}* -type f -iname ".*" -ls | awk '{print $7" bytes "$11}'
    echo ""
+   #
+   # Do we have any excludes?
+   #
+   if [ -n "$EXCLUDE" ]; then
+       echo ""
+       echo "EXCLUDE : $EXCLUDE"
+       echo ""
+       find ${DESTINATION_PATH}* -type f -name "\.*${EXCLUDE}*" -mmin -10
+       echo ""
+   fi
+   #
+   # Sleep a few secons
+   #
    echo "wait $SLEEP_SECONDS seconds for this to finish and then check again..."
    sleep $SLEEP_SECONDS
    SET_RSYNC_PROCESS_COUNT
